9e77114dc39de5940dfc717d512211a36320379e
MohammedYassin1 <Mohammed.yassin@ucalgary.ca>
2024-04-04 10:28:47 -0600

    Same as last commit

--- edu/ucalgary/oop/PROJDB.java
+++ edu/ucalgary/oop/PROJDB.java
@@ -3,13 +3,73 @@ package edu.ucalgary.oop;
 import java.sql.*;
 
 public class PROJDB {
-    private Connection conn;
-    private Statement stmt;
-    private ResultSet rs;
+    private Connection dbConnection;
 
-    public PROJDB() {
+    public void createConnection() {
         try {
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost/ensf380project", "oop", "ucalgary");
-            stmt = conn.createStatement();
+            dbConnection = DriverManager.getConnection("jdbc:postgresql://localhost/ensf380project", "oop", "ucalgary");
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void insertInquirer(String firstName, String lastName, String phoneNumber) {
+        String sql = "INSERT INTO INQUIRER (firstName, lastName, phoneNumber) VALUES (?, ?, ?)";
+
+        try (PreparedStatement pstmt = dbConnection.prepareStatement(sql)) {
+            pstmt.setString(1, firstName);
+            pstmt.setString(2, lastName);
+            pstmt.setString(3, phoneNumber);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertInquiryLog(int inquirer, Date callDate, String details) {
+        String sql = "INSERT INTO INQUIRY_LOG (inquirer, callDate, details) VALUES (?, ?, ?)";
+
+        try (PreparedStatement pstmt = dbConnection.prepareStatement(sql)) {
+            pstmt.setInt(1, inquirer);
+            pstmt.setDate(2, callDate);
+            pstmt.setString(3, details);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readInquirers() {
+        String sql = "SELECT * FROM INQUIRER";
+
+        try (Statement stmt = dbConnection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" +
+                                   rs.getString("firstName") + "\t" +
+                                   rs.getString("lastName") + "\t" +
+                                   rs.getString("phoneNumber"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readInquiryLog() {
+        String sql = "SELECT * FROM INQUIRY_LOG";
+
+        try (Statement stmt = dbConnection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" +
+                                   rs.getInt("inquirer") + "\t" +
+                                   rs.getDate("callDate") + "\t" +
+                                   rs.getString("details"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
